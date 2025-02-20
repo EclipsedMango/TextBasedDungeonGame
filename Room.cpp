14bdf18dffca514720a6d7aee7ef1aefc916b8d6
Michael <140897468+EclipsedMango@users.noreply.github.com>
2025-02-20 16:50:57 +1100

    added room descriptions

--- Room.cpp
+++ Room.cpp
@@ -5,15 +5,30 @@
 #include "SwordItem.h"
 #include "Util.h"
 
-Room::Room(std::string description, Item item) {
+Room::Room(std::string description, Item *item) {
 
 }
 
 void Room::Description() const {
+	std::vector<std::string> descriptions = {
+		"It's cold, dark, and large. Broken and old furniture litters the old room. ",
+		"Your torches light reflects off the wet and damp walls. ",
+		"As you enter a breeze follows sending a chill down your spine. ",
+		"The room is filled with old chests and antique furniture. ",
+		"A thick layer of dust and webs cover the old, decaying room. ",
+		"You hear echoes of rats scurrying across the room. ",
+		"Old moldy floor boards creek as you take a step. ",
+		"Your steps echo through out the large room. ",
+		"Old broken gear is spread throughout the dusty room. ",
+		"The room seems empty, further exploration is needed. "
+	};
 
+	std::cout << descriptions[randomInt(0, 9)];
 }
 
 void Room::enterRoom() {
+	Description();
+
 	// Generate Random Item.
 	if (randomInt(0, 2) % 2 == 0) {
 		int randomItem = randomInt(1, 3);
--- Room.h
+++ Room.h
@@ -3,9 +3,12 @@
 #include "Item.h"
 
 class Room {
-	Room(std::string description, Item item);
+	Room(std::string description, Item *item);
 	virtual ~Room() = default;
 
 	void enterRoom();
 	void Description() const;
+
+	Item *item = new Item;
+	std::string description;
 };
